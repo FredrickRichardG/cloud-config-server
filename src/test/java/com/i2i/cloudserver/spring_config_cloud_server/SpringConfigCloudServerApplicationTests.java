9a801037bb83afb4deb6aa4c0981e3701e3f6247
FredrickRichardG <fredrick.gnanaraj@ideas2it.com>
2025-05-21 18:31:07 +0530

    Add files via upload

---
+++ src/test/java/com/i2i/cloudserver/spring_config_cloud_server/SpringConfigCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i2i.cloudserver.spring_config_cloud_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
